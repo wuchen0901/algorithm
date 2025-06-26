087c340af56ff028442605d7d926726306bc6dd6
Violet Rosalia <violet.rosalia@gmail.com>
2025-06-26 23:34:43 +0900

    refactor: 547. Number of Provinces

--- 547. Number of Provinces.py
+++ 547. Number of Provinces.py
@@ -2,43 +2,24 @@ from typing import List
 
 
 class Solution:
-    def findCircleNum(self, is_connected: List[List[int]]) -> int:
-        # DFS: Starting from a node, recursively explore all its connected neighbors before backtracking.
-        # Actually, both adjacency list and adjacency matrix implicitly assume each node has an ID — usually an integer index 0 to n-1.
-        # len(isConnected) equals the number of nodes in the graph.
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        """
+        Given an adjacency matrix isConnected, return the number of connected components (provinces) in the graph.
+        """
+        def dfs(node: int):
+            for neighbor, connected in enumerate(isConnected[node]):
+                if connected and neighbor not in visited:
+                    visited.add(neighbor)
+                    dfs(neighbor)
 
-        # Example: isConnected = [
-        #     [1, 1, 0],
-        #     [1, 1, 0],
-        #     [0, 0, 1]
-        # ]
-        count = 0
+        n = len(isConnected)
         visited = set()
-        # Iterate over all unvisited node. i: 0, 1, 2
-        for i in range(len(is_connected)):
-            if i in visited:
-                continue
-            visited.add(i)
+        count = 0
 
-            # DFS through from this unvisited node and mark all as visited.
-            self.dfs(i, is_connected, visited)
-
-            count += 1
+        for i in range(n):
+            if i not in visited:
+                visited.add(i)
+                dfs(i)
+                count += 1
 
         return count
-
-    def dfs(self, curr, is_connected: List[List[int]], visited):
-        # If the current node has no more unvisited neighbors.
-        # For example, isConnected[0] is [1, 1, 0]
-        # For example, isConnected[1] is [1, 1, 0]
-        # For example, isConnected[2] is [0, 0, 1]
-        for i, v in enumerate(is_connected[curr]):
-            # If node curr is not connected to the node i, which means i is not curr's neighbor, then continue.
-            if v == 0:
-                continue
-            # If the neighbor 'i' is visited, then continue.
-            if i in visited:
-                continue
-            # If the 'i' is curr's neighbor and hasn't been visited.
-            visited.add(i)
-            self.dfs(i, is_connected, visited)
