91feb3717c60ee00ffd70da84fd159c99785c74e
Violet Rosalia <violet.rosalia@gmail.com>
2025-06-26 23:33:42 +0900

    refactor: 797. All Paths From Source to Target

--- 797. All Paths From Source to Target.py
+++ 797. All Paths From Source to Target.py
@@ -3,17 +3,17 @@ from typing import List
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        def _dfs(node: int, path: List[int]) -> None:
+            if node == len(graph) - 1:
+                result.append(path.copy())
+                return
+
+            for neighbor in graph[node]:
+                path.append(neighbor)
+                _dfs(neighbor, path)
+                path.pop()
+
         """Finds all paths from node 0 to the last node in a DAG."""
         result = []
-        self._dfs(0, [0], graph, result)
+        _dfs(0, [0])
         return result
-
-    def _dfs(self, node: int, path: List[int], graph: List[List[int]], result: List[List[int]]) -> None:
-        if node == len(graph) - 1:
-            result.append(path.copy())
-            return
-
-        for neighbor in graph[node]:
-            path.append(neighbor)
-            self._dfs(neighbor, path, graph, result)
-            path.pop()
