b1668e76de85514547010e7df743738c1b40697d
Violet Rosalia <violet.rosalia@gmail.com>
2025-06-28 01:10:01 +0900

    refactor: ordered array, two pointers

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -3,20 +3,21 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Input: nums = [-1,0,1,2,-1,-4]
+        # Output: [[-1,-1,2],[-1,0,1]]
         nums.sort()
-
+        # Input: nums = [-4,-1,-1,0,1,2]
         unique_set = set()
-
-        def backtrack(path, start):
-            if len(path) == 3:
-                if sum(path) == 0:
-                    unique_set.add(tuple(path))
-                return
-
-            for i in range(start, len(nums)):
-                path.append(nums[i])
-                backtrack(path, i + 1)
-                path.pop()
-
-        backtrack([], 0)
-        return [list(result) for result in unique_set]
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+            while left != right:
+                total = nums[left] + nums[right]
+                if total < -nums[i]:
+                    left += 1
+                elif -nums[i] < total:
+                    right -= 1
+                else:
+                    unique_set.add(tuple([nums[i], nums[left], nums[right]]))
+                    left += 1
+        return [list(r) for r in unique_set]
