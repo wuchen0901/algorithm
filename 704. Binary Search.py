0c59f925ef9f3fbe226e1935db3c9a771ac1305c
Violet Rosalia <violet.rosalia@gmail.com>
2025-07-03 11:28:52 +0900

    feat: binary search left

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -18,3 +18,20 @@ class Solution:
                 return mid
 
         return -1
+
+    def binary_search_left(self, nums: List[int], target: int) -> int:
+        # [1, 4, 4, 4, 4, 5, 5, 5, 6, 7, 7, 8]
+        #  0  1  2  3  4  5  6  7  8  9 10 11
+        left, right = 0, len(nums) - 1
+        result = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                result = mid
+                right = mid - 1
+
+        return result
