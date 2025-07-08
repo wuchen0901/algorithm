1429314f78afcc3a958da63bb23763ed19aefa81
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-07-08 11:09:11 +0900

    feat: bottom-up DP

--- 494. Target Sum.py
+++ 494. Target Sum.py
@@ -15,3 +15,18 @@ class Solution:
             return dfs(pre_sum + nums[i], i + 1) + dfs(pre_sum - nums[i], i + 1)
 
         return dfs(0, 0)
+
+    def findTargetSumWays_bottom_up(self, nums: List[int], target: int) -> int:
+        total = sum(nums)
+        if (total + target) % 2 != 0 or abs(target) > total:
+            return 0
+
+        subset_sum = (total + target) // 2
+        dp = [0] * (subset_sum + 1)
+        dp[0] = 1
+
+        for num in nums:
+            for j in range(subset_sum, num - 1, -1):
+                dp[j] += dp[j - num]
+
+        return dp[subset_sum]
