7ecb28786af4fd136d170b8d72f9d3f75fd54392
Violet Rosalia <violet.rosalia@gmail.com>
2025-07-23 13:57:04 +0900

    refactor: solve LIS problem using binary search (bisect_left)

--- graph/300. Longest Increasing Subsequence.py
+++ graph/300. Longest Increasing Subsequence.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from typing import List
 
 
@@ -6,40 +7,41 @@ class Solution:
     # Output: 4
     # Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # dynamic programming problem
-        # dp = {}
-        # for num in nums:
-        #     for v in dp.items():
-        #        if v < num:
-        #            dp[v]
-        # If I write it this way, things will go wrong when the input contains duplicate elements
-        # so the inputs can’t be used as hash keys
-        # At last, I
-        # Input: nums = [10,9,2,5,3,7,101,18]
-        # I need to maintain an relationship
-        # read 10
-        # [10] ,[1]
-        # read 9
-        # [10,9], [1,1]
-        # 2
-        # [10,9,2],  [1,1,1]
-        # 5
-        # [10,9,2,5],  [1,1,1,2]
-        # 3
-        # [10,9,2,5,3],  [1,1,1,2,2]
-        # 7
-        # [10,9,2,5,3,7],  [1,1,1,2,2,3]
-        # 101
-        # [10,9,2,5,3,7,101],  [1,1,1,2,2,3,4]
-        # 18
-        # [10,9,2,5,3,7,101,18],  [1,1,1,2,2,3,4,4]
-        # 4
-        # Input: nums = [10,9,2,5,3,7,101,18]
-        counts = []
-        for i, num in enumerate(nums):
-            max_count = 1
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    max_count = max(max_count, counts[j] + 1)
-            counts.append(max_count)
-        return max(counts)
+        tails = [float('inf')] * len(nums)
+        # tails = [0,0,0,0,0,0,0,0]
+        length = 0
+        for num in nums:
+            index = bisect_left(tails, num)
+            # 0
+            # 0
+            # 0
+            # 1
+            # 1
+            # 2
+            # 3
+            # 3
+            tails[index] = num
+            # [10,0,0,0, 0,0,0,0]
+            # [ 9,0,0,0, 0,0,0,0]
+            # [ 2,0,0,0, 0,0,0,0]
+            # [ 2,5,0,0, 0,0,0,0]
+            # [ 2,3,0,0, 0,0,0,0]
+            # [ 2,3,7,0, 0,0,0,0]
+            # [ 2,3,7,101, 0,0,0,0]
+            # [ 2,3,7,18, 0,0,0,0]
+            length = max(length, index)
+            # 0
+            # 0
+            # 0
+            # 1
+            # 1
+            # 2
+            # 3
+            # 3
+
+        return length + 1
+
+
+if __name__ == '__main__':
+    index = bisect_left([1, 2, 2, 3], 1.5, )
+    print(index)
