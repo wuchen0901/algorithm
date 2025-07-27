ddd8066df9c54de15eb25b55571b69ae6ea1448f
Violet Rosalia <violet.rosalia@gmail.com>
2025-07-27 14:49:37 +0900

    refactor: 3 solutions for the backtracking with duplicate elements problem

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -16,8 +16,7 @@ class Solution:
 
             while i < len(remaining):
                 path.append(remaining[i])
-                rest = remaining[:i] + remaining[i + 1:]
-                backtrack(path, rest)
+                backtrack(path, remaining[:i] + remaining[i + 1:])
                 path.pop()
 
                 i = bisect_right(remaining, remaining[i])
@@ -25,6 +24,55 @@ class Solution:
         backtrack([], nums)
 
         return permutations
+
+    def permuteUnique_used_array(self, nums: List[int]) -> List[List[int]]:
+        """
+        Method 2: Use 'used' array + sorting + pruning to avoid duplicates
+        """
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue  # Prune: avoid choosing the same element in the same depth
+                used[i] = True
+                path.append(nums[i])
+                backtrack(path)
+                path.pop()
+                used[i] = False
+
+        backtrack([])
+        return res
+
+    def permuteUnique_counter(self, nums: List[int]) -> List[List[int]]:
+        """
+        Method 3: Use Counter to track element usage frequency
+        """
+        from collections import Counter
+        counter = Counter(nums)
+        res = []
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            for num in counter:
+                if counter[num] > 0:
+                    counter[num] -= 1
+                    path.append(num)
+                    backtrack(path)
+                    path.pop()
+                    counter[num] += 1
+
+        backtrack([])
+        return res
 
 
 if __name__ == '__main__':
