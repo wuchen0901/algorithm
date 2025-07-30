7071664a916612897d86e06c70346f3193a1b749
Violet Rosalia <violet.rosalia@gmail.com>
2025-07-30 15:21:59 +0900

    refactor: Binary Tree Level-Order Traversal

---
+++ 102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,30 @@
+from collections import deque
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue = deque([root])
+        result = []
+
+        while queue:
+            # 记录当前层
+            result.append([n.val for n in queue])
+
+            # 弹出当前层并压入下一层
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return result
