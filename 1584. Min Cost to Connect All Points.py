391dce29ed1e84be360368c91d9a10f22f34e4b2
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-07-31 12:34:24 +0900

    feat: solve MST with Kruskal’s algorithm

--- 1584. Min Cost to Connect All Points.py
+++ 1584. Min Cost to Connect All Points.py
@@ -31,3 +31,47 @@ class Solution:
                     heapq.heappush(heap, (weight, neighbor))
 
         return cost
+
+
+# Kruskal's algorithm implementation
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, x):
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, x, y):
+        fx, fy = self.find(x), self.find(y)
+        if fx == fy:
+            return False
+        self.parent[fx] = fy
+        return True
+
+
+class SolutionKruskal:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        edges = []
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((dist, i, j))
+
+        edges.sort()
+        uf = UnionFind(n)
+        cost = 0
+        count = 0
+
+        for dist, u, v in edges:
+            if uf.union(u, v):
+                cost += dist
+                count += 1
+                if count == n - 1:
+                    break
+
+        return cost
