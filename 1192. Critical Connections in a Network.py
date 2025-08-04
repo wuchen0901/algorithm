5b97d8839ebfa9a36a933c9ce04fa1a6b2b2ed12
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-08-04 16:00:01 +0900

    feat: Tarjan Algorithm
    refactor: debug criticalConnections
    
    feat: Tarjan Algorithm
    
    refactor: debug criticalConnections
    
    refactor: criticalConnections

--- 1192. Critical Connections in a Network.py
+++ 1192. Critical Connections in a Network.py
@@ -5,37 +5,34 @@ class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
         from collections import defaultdict
 
-        def dfs(u: int, parent: int):
-            nonlocal time
-            visited[u] = True
-            dfn[u] = low[u] = time
-            time += 1
-
-            for v in graph[u]:
-                if v == parent:
-                    continue
-                if not visited[v]:
-                    dfs(v, u)
-                    low[u] = min(low[u], low[v])
-                    if low[v] > dfn[u]:
-                        result.append([u, v])
-                else:
-                    low[u] = min(low[u], dfn[v])
-
-        # Build graph
         graph = defaultdict(list)
         for u, v in connections:
             graph[u].append(v)
             graph[v].append(u)
 
-        dfn = [0] * n          # Discovery time of each node
-        low = [0] * n          # Lowest discovery time reachable
-        visited = [False] * n
-        time = 1
-        result = []
+        discovery_time = [-1] * n  # When each node is first visited
+        earliest_reachable = [-1] * n  # The lowest discovery time reachable from this node
+        bridges = []
+        time = 0
+
+        def dfs(node: int, parent: int):
+            nonlocal time
+            discovery_time[node] = earliest_reachable[node] = time
+            time += 1
+
+            for neighbor in graph[node]:
+                if neighbor == parent:
+                    continue
+                if discovery_time[neighbor] == -1:
+                    dfs(neighbor, node)
+                    earliest_reachable[node] = min(earliest_reachable[node], earliest_reachable[neighbor])
+                    if earliest_reachable[neighbor] > discovery_time[node]:
+                        bridges.append([node, neighbor])
+                else:
+                    earliest_reachable[node] = min(earliest_reachable[node], discovery_time[neighbor])
 
         for i in range(n):
-            if not visited[i]:
+            if discovery_time[i] == -1:
                 dfs(i, -1)
 
-        return result
+        return bridges
