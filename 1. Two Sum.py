88f5a0a77f89f6e52eddb3b8de1f8c8a627cf331
Violet Rosalia <violet.rosalia@gmail.com>
2025-08-15 16:43:18 +0900

    1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,17 +1,11 @@
-from typing import List, Dict
+from typing import List
 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-        Given an array of integers `nums` and an integer `target`, return the indices of the two numbers such that they add up to `target`.
-        Assumes exactly one solution exists.
-        """
-        index_map: Dict[int, int] = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in index_map:
-                return [index_map[complement], i]
-            index_map[num] = i
-
-        raise ValueError("No two sum solution exists")
+        complement = {}
+        for i, n in enumerate(nums):
+            if target - n in complement:
+                return [complement[target - n], i]
+            complement[n] = i
+        return None
