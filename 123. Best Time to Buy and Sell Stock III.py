6355fbe34231b0cc785f6a6a4779a2f1439399d6
Violet Rosalia <violet.rosalia@gmail.com>
2025-08-21 12:25:19 +0900

    123. Best Time to Buy and Sell Stock III.py

--- 123. Best Time to Buy and Sell Stock III.py
+++ 123. Best Time to Buy and Sell Stock III.py
@@ -7,7 +7,7 @@ class Solution:
         hold = [-float("inf")] * (k + 1)
         cash = [0] * (k + 1)
         for price in prices:
-            for i in range(k, -1, -1):
+            for i in range(k, 0, -1):
                 hold[i - 1] = max(hold[i - 1], cash[i] - price)
                 cash[i - 1] = max(cash[i - 1], hold[i - 1] + price)
         return cash[0]
@@ -24,4 +24,4 @@ class Solution:
         #   0,2     *-2,1
 
 
-print(Solution().maxProfit([1, 2, 3, 4, 5]))
+print(Solution().maxProfit([3,3,5,0,0,3,1,4]))
