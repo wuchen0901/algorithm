f5fa8909222d1e47c11c0f615c51d1274c66f7d4
Violet Rosalia <violet.rosalia@gmail.com>
2025-08-21 11:51:53 +0900

    123. Best Time to Buy and Sell Stock III.py

--- 123. Best Time to Buy and Sell Stock III.py
+++ 123. Best Time to Buy and Sell Stock III.py
@@ -1,18 +1,15 @@
 from typing import List
-import unittest
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         k = 2
-        hold = [-float('inf')] * k  # [-inf, -inf]
-        hold[k - 1] = -prices[0]  # [-inf, -1]
-        cash = [0] * (k + 1)  # [0, 0, 0]
-        for i in range(1, len(prices)):  # - - - - - - - - 1  2  3  4  5
-            hold[1] = max(hold[1], cash[2] - prices[i])  #-1 -1 -1 -1 -1
-            cash[1] = max(cash[1], hold[1] + prices[i])  # 0  1  2  3  4
-            hold[0] = max(hold[0], cash[1] - prices[i])  # - -1 -1 -1 -1
-            cash[0] = max(cash[0], hold[0] + prices[i])  # 0  1  2  3  4
+        hold = [-float("inf")] * (k + 1)
+        cash = [0] * (k + 1)
+        for price in prices:
+            for i in range(k, -1, -1):
+                hold[i - 1] = max(hold[i - 1], cash[i] - price)
+                cash[i - 1] = max(cash[i - 1], hold[i - 1] + price)
         return cash[0]
         # k      hold                            cash
         # 0      max(hold[0], cash[1] - price)   max(cash[0], hold[0] + price)
