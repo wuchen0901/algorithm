9926e5e7edeb5496fc5fcf4f7f9ea02efee7449d
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-09-18 14:39:32 +0900

    188. Best Time to Buy and Sell Stock IV.py

--- 188. Best Time to Buy and Sell Stock IV.py
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -3,13 +3,17 @@ from typing import List
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        hold = [-float("inf")] * (k + 1)
-        cash = [0] * (k + 1)
-        for price in prices:
-            for i in range(k, 0, -1):
-                hold[i - 1] = max(hold[i - 1], cash[i] - price)
-                cash[i - 1] = max(cash[i - 1], hold[i - 1] + price)
-        return cash[0]
+        n = len(prices)
+        MAX_TRANSACTION_COUNT = k
+        HOLD, SOLD = 0, 1
+        dp = [[[-float('inf'), 0] for _ in range(MAX_TRANSACTION_COUNT + 1)] for _ in range(n + 1)]
+
+        for i in range(len(prices)):
+            for count in range(MAX_TRANSACTION_COUNT):
+                dp[i + 1][count + 1][HOLD] = max(dp[i][count + 1][HOLD], dp[i][count][SOLD] - prices[i])
+                dp[i + 1][count + 1][SOLD] = max(dp[i][count + 1][SOLD], dp[i + 1][count + 1][HOLD] + prices[i])
+
+        return dp[n][k][SOLD]
 
 
 print(Solution().maxProfit(2, [3, 5, 0, 3, 1, 4]))
