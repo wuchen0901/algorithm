35e6badc322746766269bc7bff6e65e91ce19dd3
Violet Rosalia <violet.rosalia@gmail.com>
2025-09-20 10:07:37 +0900

    416. Partition Equal Subset Sum.py

--- 416. Partition Equal Subset Sum.py
+++ 416. Partition Equal Subset Sum.py
@@ -3,14 +3,15 @@ from typing import List
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        nums.sort()
         total = sum(nums)
         if total % 2:
             return False
         half_sum = total // 2
         bits = 1
         for n in nums:
-            if half_sum < n:
-                continue
+            if n > half_sum:
+                break  # 已排序，后面都更大
             bits |= bits << n
             bits &= (1 << (half_sum + 1)) - 1
             if bits & (1 << half_sum):
