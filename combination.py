7dde244987a4e00b6e23e46c298efdea0b5e8f4d
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-09-29 11:33:11 +0900

    combination.py

--- combination.py
+++ combination.py
@@ -1,14 +1,22 @@
-class Solution:
-    def subsets(self, nums):
-        res = []
+from typing import List
 
-        def backtrack(start, path):
-            res.append(path[:])  # 每一步都存当前组合
 
-            for i in range(start, len(nums)):
-                path.append(nums[i])
-                backtrack(i + 1, path)
-                path.pop()
+# Given a list of unique integers, return all possible subsets (the power set).
+# Example:
+#   Input:  nums = [3, 6, 7]
+#   Output: [[], [3], [3, 6], [3, 6, 7], [3, 7], [6], [6, 7], [7]]
+def subsets(nums: List[int]) -> List[List[int]]:
+    res = []
 
-        backtrack(0, [])
-        return res
+    def backtrack(curr: List[int], index: int):
+        res.append(curr.copy())
+
+        for i in range(index, len(nums)):
+            curr.append(nums[i])
+            backtrack(curr, i + 1)
+            curr.pop()
+
+    backtrack([], 0)
+    return res
+
+print(subsets([3, 6, 7]))
