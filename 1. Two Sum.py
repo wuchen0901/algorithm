7e34126382347925df6c421603c1b40b2340386d
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-10-10 15:34:31 +0900

    1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -4,8 +4,8 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         complement = {}
-        for i, n in enumerate(nums):
-            if target - n in complement:
-                return [complement[target - n], i]
-            complement[n] = i
-        return None
+        for i, num in enumerate(nums):
+            if target - num in complement:
+                return [complement[target - num], i]  # earlier index, then current
+            else:
+                complement[num] = i
