5f768b0bec1f006391c62258c71c2b1a911efb8b
Violet Rosalia <violet.rosalia@gmail.com>
2025-10-14 15:08:32 +0900

    209. Minimum Size Subarray Sum

--- java/grammar_review.md
+++ java/grammar_review.md
@@ -5,3 +5,5 @@
 | 树只能执行基于树的搜索                         | tree can only tree search                  | tree can only perform tree-based searches               |
 | 遵循编码规范                                  | obey coding convention                     | follow the coding conventions                           |
 | 在处理窗口区间时采用左闭右开表示法            | when operate a window, use [left, right), left include, right exclude | when operating on a window, use the half-open interval [left, right) (inclusive of left endpoint, exclusive of right endpoint) |
+| 在进行编程面试时                              | when doing coding interview                | during a coding interview                               |
+| 退出 while 循环之后                           | When quit while loop                       | After exiting the while loop                            |
--- java/src/main/java/com/leetcode/LeetCode_209_Minimum_Size_Subarray_Sum.java
+++ java/src/main/java/com/leetcode/LeetCode_209_Minimum_Size_Subarray_Sum.java
@@ -1,7 +1,33 @@
 package com.leetcode;
 
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
+
+    public int minSubArrayLenV2(int target, int[] nums) {
+        long sum = 0;
+        int l = 0, r = 0;
+        int minimalLength = Integer.MAX_VALUE;
+        while (r < nums.length) {
+            sum += nums[r];
+            r++;
+            // Enter the loop when
+            // [2,3,1,2,4,3]
+            //          ^
+            // I am following the half-open interval convention,
+            // which means the left endpoint is inclusive and the right endpoint is exclusive.
+            while (target <= sum) {
+                minimalLength = Math.min(minimalLength, r - l);
+                sum -= nums[l];
+                l++;
+            }
+            // After exiting the while loop, sum < target
+            // [2,3,1,2,4,3]
+            //    ^     ^
+        }
+
+        return minimalLength == Integer.MAX_VALUE ? 0 : minimalLength;
+    }
+
+    public int minSubArrayLenV1(int target, int[] nums) {
         int sum = 0;
         int l = 0;
         int r = 0;
@@ -11,8 +37,11 @@ class Solution {
                 sum += nums[r];
                 r++;
             }
+            // Enter the loop when
             // [2,3,1,2,4,3]
             //          ^
+            // I am following the half-open interval convention,
+            // which means the left endpoint is inclusive and the right endpoint is exclusive.
             while (target <= sum) {
                 if (r - l < minimalLength) {
                     minimalLength = r - l;
