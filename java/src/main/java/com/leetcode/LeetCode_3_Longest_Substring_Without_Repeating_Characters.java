c7694b9f7aebf7662069e8aa4ddc0e6abda3e5de
Violet Rosalia <violet.rosalia@gmail.com>
2025-10-14 22:29:46 +0900

    3. Longest Substring Without Repeating Characters

--- java/src/main/java/com/leetcode/LeetCode_3_Longest_Substring_Without_Repeating_Characters.java
+++ java/src/main/java/com/leetcode/LeetCode_3_Longest_Substring_Without_Repeating_Characters.java
@@ -2,17 +2,42 @@ package com.leetcode;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class LeetCode_3_Longest_Substring_Without_Repeating_Characters {
-    // template like but incorrect
+    // expand first
+    public int lengthOfLongestSubstringV3(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+        int l = 0;
+        int longestLength = 0;
+
+        for (int r = 0; r < s.length(); ) {
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            r++;
+            // "abcabcbb"
+            //  ^  ^
+            // move left until it doesn't contain duplicates
+            while (1 < window.getOrDefault(c, 0)) {
+                // update window
+                window.computeIfPresent(s.charAt(l), (_, count) -> count == 1 ? null : count - 1);
+                l++;
+            }
+            // "abcabcbb"
+            //   ^ ^
+            longestLength = Math.max(longestLength, r - l);
+        }
+
+        return longestLength;
+    }
+
+    // Shrink first
     public int lengthOfLongestSubstringV2(String s) {
         Set<Character> window = new HashSet<>();
         int l = 0;
         int longestLength = 0;
         for (int r = 0; r < s.length(); r++) {
-            longestLength = Math.max(longestLength, r - l);
-
             // move left until it doesn't contain
             while (window.contains(s.charAt(r))) {
                 // update window
@@ -20,7 +45,7 @@ public class LeetCode_3_Longest_Substring_Without_Repeating_Characters {
                 l++;
             }
             window.add(s.charAt(r));
-
+            longestLength = Math.max(longestLength, r - l + 1);
         }
 
         return longestLength;
