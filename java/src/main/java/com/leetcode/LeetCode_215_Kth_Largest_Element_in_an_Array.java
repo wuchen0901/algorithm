4054aaec9b9fac1f22fa12e03d0dbf5976896ae6
Violet Rosalia <violet.rosalia@gmail.com>
2025-11-12 12:30:50 +0900

    Optimize kth largest heap

--- java/src/main/java/com/leetcode/LeetCode_215_Kth_Largest_Element_in_an_Array.java
+++ java/src/main/java/com/leetcode/LeetCode_215_Kth_Largest_Element_in_an_Array.java
@@ -1,18 +1,16 @@
 package com.leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class LeetCode_215_Kth_Largest_Element_in_an_Array {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
-        priorityQueue.addAll(Arrays.stream(nums).boxed().toList());
-        int result = 0;
-        for (int i = 0; i < k; i++) {
-            result = priorityQueue.poll();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            minHeap.offer(nums[i]);
+            if (k < minHeap.size()) {
+                minHeap.poll();
+            }
         }
-
-        return result;
+        return minHeap.peek();
     }
 }
