96eacc78226955e562e5aa4bf419a2d024c949e7
Chen.A.Wu <Chen.A.Wu@sony.com>
2025-11-21 15:57:56 +0900

    Refactor top k frequent elements solution

--- java/src/main/java/com/leetcode/LeetCode_347_Top_K_Frequent_Elements.java
+++ java/src/main/java/com/leetcode/LeetCode_347_Top_K_Frequent_Elements.java
@@ -6,22 +6,24 @@ import java.util.PriorityQueue;
 
 public class LeetCode_347_Top_K_Frequent_Elements {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> counter = new HashMap<>();
-        for (int num : nums) {
-            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
         }
 
-        // Max-heap based on frequency
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
-        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
-            queue.add(new int[]{entry.getValue(), entry.getKey()});
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            minHeap.offer(entry);
+            if (k < minHeap.size()) {
+                minHeap.poll();
+            }
         }
 
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            result[i] = queue.poll()[1];
+            result[i] = minHeap.poll().getKey();
         }
-
         return result;
     }
 }
