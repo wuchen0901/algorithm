0d8c7183d54b1d0012941168bb66e992b469fd94
Violet Rosalia <violet.rosalia@gmail.com>
2026-02-21 21:32:48 +0900

    322. Coin Change

--- java/src/main/java/com/leetcode/LeetCode_322_Coin_Change.java
+++ java/src/main/java/com/leetcode/LeetCode_322_Coin_Change.java
@@ -2,35 +2,29 @@ package com.leetcode;
 
 public class LeetCode_322_Coin_Change {
     public int coinChange(int[] coins, int amount) {
-        int inf = amount + 1;
         int[][] dp = new int[coins.length + 1][amount + 1];
-        for (int j = 1; j < dp[0].length; j++) {
+
+        int inf = amount + 1;
+
+        for (int j = 0; j < amount + 1; j++) {
             dp[0][j] = inf;
         }
-
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[i].length; j++) {
-                int coin = coins[i - 1];
-                if (j - coin >= 0) {
-                    dp[i][j] = Math.min(dp[i][j - coin] + 1, dp[i - 1][j]);
+        //              0   1   2   3   4   5   6
+        // 0 []
+        // 1 [2]        0  +∞   1  +∞   2  +∞   2
+        // 2 [2, 1]
+        // 3 [2, 1, 5]
+        for (int i = 1; i < coins.length + 1; i++) {
+            int coin = coins[i - 1];
+            for (int j = 1; j < amount + 1; j++) {
+                if (0 <= j - coin) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coin] + 1);
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        return dp[coins.length][amount] >= inf ? -1 : dp[coins.length][amount];
-    }
-
-    public static void main(String[] args) {
-        int[][] ints = new int[3][5];
-        int index = 0;
-        for (int i = 0; i < ints.length; i++) {
-            for (int j = 0; j < ints[i].length; j++) {
-                ints[i][j] = index;
-                index++;
-            }
-        }
-        System.out.println(ints);
+        return dp[coins.length][amount] < inf ? dp[coins.length][amount] : -1;
     }
 }
