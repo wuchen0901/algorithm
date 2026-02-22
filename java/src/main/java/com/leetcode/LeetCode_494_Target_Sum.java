d37bcbb67c3d5992edf4342d1d926a87587eab6a
Violet Rosalia <violet.rosalia@gmail.com>
2026-02-22 15:43:58 +0900

    494. Target Sum

--- java/src/main/java/com/leetcode/LeetCode_494_Target_Sum.java
+++ java/src/main/java/com/leetcode/LeetCode_494_Target_Sum.java
@@ -4,37 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LeetCode_494_Target_Sum {
-
+    // todo 最标准（面试里最常见、最“模板化”）的解法：把 Target Sum 转成子集和计数问题（0/1 背包计数），再用 1D DP。
     public int findTargetSumWays2(int[] nums, int target) {
+        // index        0   1   2   3   4   5   6
+        //              -3  -2  -1  0   1   2   3   4   5   6   7
+        //          []              1
+        //         [1]          1       1
+        //      [1, 1]      1       2       1
+        //   [1, 1, 1]  1       3       3       1
+
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
 
-        if (sum < Math.abs(target)){
+        if (sum < Math.abs(target)) {
             return 0;
         }
 
-        final int offset = sum;
-        final int width = 2 * sum + 1;
+        int offset = sum;
 
-        int[][] dp = new int[nums.length + 1][width];
-        dp[0][offset] = 1; // using 0 numbers, only sum=0 is reachable
+        int[][] dp = new int[nums.length + 1][sum * 2 + 1];
 
-        for (int i = 1; i <= nums.length; i++) {
+        dp[0][0 + offset] = 1;
+
+        for (int i = 1; i < nums.length + 1; i++) {
             int num = nums[i - 1];
-            for (int j = 0; j < width; j++) {
-                // j = 0, 1, 2, 3
-
-                // nums = [2, 2]
-                // sum = 4,
-                // 0   1  2  3  4 5 6 7 8
-                // -4 -3 -2 -1  0 1 2 3 4
-                //
-                //              1
-                //        1         1
-                // 1            2       1
-                if (j + num < width) {
+            for (int j = 0; j < sum * 2 + 1; j++) {
+                if (j + num < sum * 2 + 1) {
                     dp[i][j] += dp[i - 1][j + num];
                 }
                 if (0 <= j - num) {
