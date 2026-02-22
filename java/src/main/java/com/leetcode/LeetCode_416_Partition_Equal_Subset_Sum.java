688266a11959187e0c171c37120fd4c9ae8279ac
Violet Rosalia <violet.rosalia@gmail.com>
2026-02-22 14:56:49 +0900

    416. Partition Equal Subset Sum

--- java/src/main/java/com/leetcode/LeetCode_416_Partition_Equal_Subset_Sum.java
+++ java/src/main/java/com/leetcode/LeetCode_416_Partition_Equal_Subset_Sum.java
@@ -3,39 +3,38 @@ package com.leetcode;
 public class LeetCode_416_Partition_Equal_Subset_Sum {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for (int num : nums) {
-            sum += num;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
 
-        if (sum % 2 == 1) { // odd
+        if (sum %2 == 1) {
             return false;
         }
 
-        int[][] dp = new int[nums.length + 1][sum / 2 + 1];
+        int target = sum / 2;
 
-        for (int i = 0; i < nums.length; i++) {
-            dp[i][0] = 1;
-        }
+        int[][] dp = new int[nums.length + 1][target + 1];
 
-        // todo  背包问题不是用dfs解决的吗？
-        //
-        // 1 5 5  -  11
-        //     0 1 2 3 4 5 6 7 8 9 10
-        //     1
-        // 0 3       1
-        // 1 2     1     1
-        // 2 4         1   1     1
-        for (int i = 1; i <= nums.length; i++) {
+        dp[0][0] = 1;
+
+        //              0   1   2   3   4   5   6
+        //  []          1 // TODO: 为什么这里是 1 ？
+        //  [2]         1       1
+        //  [2, 3]      1       1   1       1
+        //  [2, 3, 1]   1   1   1   2   1   1   1
+        for (int i = 1; i < nums.length + 1; i++) {
             int num = nums[i - 1];
-            for (int j = 1; j <= sum / 2; j++) {
+            for (int j = 0; j < target + 1; j++) {
                 if (0 <= j - num) {
                     dp[i][j] = dp[i - 1][j] + dp[i - 1][j - num];
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
+
             }
         }
 
-        return dp[nums.length][sum / 2] != 0;
+        return dp[nums.length][target] != 0;
     }
 }
