5628991cdc5635a19457e4d0d580281c85deceb3
Chen.A.Wu <Chen.A.Wu@sony.com>
2026-03-03 18:50:03 +0900

    377. Combination Sum IV

--- java/src/main/java/com/leetcode/LeetCode_377_Combination_Sum_IV.java
+++ java/src/main/java/com/leetcode/LeetCode_377_Combination_Sum_IV.java
@@ -2,22 +2,18 @@ package com.leetcode;
 
 public class LeetCode_377_Combination_Sum_IV {
     public int combinationSum4(int[] nums, int target) {
-        int[][] dp = new int[nums.length + 1][target + 1];
+        int[] dp = new int[target + 1];
 
-        for (int i = 0; i < nums.length + 1; i++) {
-            dp[i][0] = 1;
-        }
+        dp[0] = 1;
 
-        for (int i = 1; i < nums.length + 1; i++) {
-            for (int j = 1; j < target + 1; j++) {
-                for (int n : nums) {
-                    if (0 <= j - n) {
-                        dp[i][j] += dp[i][j - n];
-                    }
+        for (int i = 1; i < target + 1; i++) {
+            for (int n : nums) {
+                if (0 <= i - n) {
+                    dp[i] += dp[i - n];
                 }
             }
         }
 
-        return dp[nums.length][target];
+        return dp[target];
     }
 }
