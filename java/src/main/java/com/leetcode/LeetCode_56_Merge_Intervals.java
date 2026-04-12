5c61dd65aeea3806b22a5cdb34b3caf45cfe241e
Violet Rosalia <violet.rosalia@gmail.com>
2026-04-12 18:13:48 +0900

    56. Merge Intervals

--- java/src/main/java/com/leetcode/LeetCode_56_Merge_Intervals.java
+++ java/src/main/java/com/leetcode/LeetCode_56_Merge_Intervals.java
@@ -2,35 +2,31 @@ package com.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class LeetCode_56_Merge_Intervals {
     public int[][] merge(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return new int[0][2];
-        }
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        List<int[]> mergedIntervals = new ArrayList<>();
 
-        // 1) sort by start
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int start = intervals[0][0];
+        int end = intervals[0][1];
 
-        List<int[]> merged = new ArrayList<>();
-        int curStart = intervals[0][0];
-        int curEnd = intervals[0][1];
-
-        // 2) scan and merge
         for (int i = 1; i < intervals.length; i++) {
-            int s = intervals[i][0], e = intervals[i][1];
-            if (s <= curEnd) {           // overlap
-                curEnd = Math.max(curEnd, e);
-            } else {                     // no overlap
-                merged.add(new int[]{curStart, curEnd});
-                curStart = s;
-                curEnd = e;
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            if (end < currStart) {
+                mergedIntervals.add(new int[]{start, end});
+                start = currStart;
+                end = currEnd;
+            } else {
+                end = Math.max(end, currEnd);
             }
         }
 
-        // 3) add the last window
-        merged.add(new int[]{curStart, curEnd});
-        return merged.toArray(new int[merged.size()][]);
+        mergedIntervals.add(new int[]{start, end});
+
+        return mergedIntervals.toArray(new int[0][]);
     }
 }
