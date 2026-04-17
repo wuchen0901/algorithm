ffb1f9f261a229d8f2a300684712324a5526e7c6
Violet Rosalia <violet.rosalia@gmail.com>
2026-04-17 12:15:34 +0900

    33. Search in Rotated Sorted Array

---
+++ java/src/main/java/com/leetcode/LeetCode_33_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+public class LeetCode_33_Search_in_Rotated_Sorted_Array {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] <= nums[right]) {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
