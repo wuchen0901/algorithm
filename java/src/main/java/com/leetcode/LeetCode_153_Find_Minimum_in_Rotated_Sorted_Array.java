bf0bd257e67a071eb6263bc475924069970b8d3c
Violet Rosalia <violet.rosalia@gmail.com>
2026-04-17 15:31:15 +0900

    153. Find Minimum in Rotated Sorted Array

--- java/src/main/java/com/leetcode/LeetCode_153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ java/src/main/java/com/leetcode/LeetCode_153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -11,23 +11,11 @@ public class LeetCode_153_Find_Minimum_in_Rotated_Sorted_Array {
             // 1 2 3 4 5
             // 5 1 2 3 4
             // 4 5 1 2 3
-            if (nums[mid] < nums[right]) {
+            if (nums[mid] <= nums[right]) {
                 right = mid;
-            } else
-
-                // 9
-                // 1 2 3 4 5
-                // 3 4 5 1 2
-                // 2 3 4 5 1
-                if (nums[left] <= nums[mid]) {
-                    // 1 2 3 4 5
-                    if (nums[left] < nums[right]) {
-                        return left;
-                        // 3 4 5 1 2
-                    } else if (nums[left] > nums[right]) {
-                        left = mid + 1;
-                    }
-                }
+            } else {
+                left = mid + 1;
+            }
         }
 
         return nums[left];
