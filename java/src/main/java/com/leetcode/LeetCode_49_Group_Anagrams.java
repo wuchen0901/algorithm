db83f0a9be64c94bbf57863c3256089f120d0867
Violet Rosalia <violet.rosalia@gmail.com>
2026-04-22 22:39:07 +0900

    49. Group Anagrams

---
+++ java/src/main/java/com/leetcode/LeetCode_49_Group_Anagrams.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+import java.util.*;
+
+public class LeetCode_49_Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> group = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            group.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(group.values());
+    }
+}
