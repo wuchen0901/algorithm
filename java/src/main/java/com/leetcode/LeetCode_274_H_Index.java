0c9f4deefb613d33db2edb18abf1924094c610f8
Violet Rosalia <violet.rosalia@gmail.com>
2026-04-22 18:55:21 +0900

    274. H-Index

--- java/src/main/java/com/leetcode/LeetCode_274_H_Index.java
+++ java/src/main/java/com/leetcode/LeetCode_274_H_Index.java
@@ -6,23 +6,10 @@ public class LeetCode_274_H_Index {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
 
-        int h = citations.length;
-        while (0 <= h) {
-            boolean qualified = true;
-            for (int i = citations.length - 1; citations.length - h <= i; i--) {
-                if (i + 1 <= citations[i]) {
-
-                } else {
-                    qualified = false;
-                    break;
-                }
-            }
-
-            if (qualified) {
+        for (int h = citations.length; 1 <= h; h--) {
+            if (h <= citations[citations.length - h]) {
                 return h;
             }
-
-            h--;
         }
 
         return 0;
