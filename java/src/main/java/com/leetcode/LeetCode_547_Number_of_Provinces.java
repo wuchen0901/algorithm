8d7ee0dbe2f4d1db64fceaaf594454a93102d405
Chen.A.Wu <Chen.A.Wu@sony.com>
2026-05-07 15:53:01 +0900

    547. Number of Provinces BFS

--- java/src/main/java/com/leetcode/LeetCode_547_Number_of_Provinces.java
+++ java/src/main/java/com/leetcode/LeetCode_547_Number_of_Provinces.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LeetCode_547_Number_of_Provinces {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
@@ -24,5 +27,31 @@ public class LeetCode_547_Number_of_Provinces {
                 dfs(isConnected, visited, next);
             }
         }
+    }
+
+    public int findCircleNumBfs(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinceCount = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            provinceCount++;
+            queue.offer(i);
+            visited[i] = true;
+            while (!queue.isEmpty()) {
+                int city = queue.poll();
+                for (int neighbor = 0; neighbor < n; neighbor++) {
+                    if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.offer(neighbor);
+                    }
+                }
+            }
+        }
+
+        return provinceCount;
     }
 }
