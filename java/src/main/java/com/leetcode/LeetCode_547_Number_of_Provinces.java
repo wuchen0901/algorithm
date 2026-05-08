f2146ef4b934c2e7f14003efe3b1f166b0a0fe89
Violet Rosalia <violet.rosalia@gmail.com>
2026-05-08 15:01:22 +0900

    547. Number of Provinces UnionFind

--- java/src/main/java/com/leetcode/LeetCode_547_Number_of_Provinces.java
+++ java/src/main/java/com/leetcode/LeetCode_547_Number_of_Provinces.java
@@ -54,4 +54,69 @@ public class LeetCode_547_Number_of_Provinces {
 
         return provinceCount;
     }
+
+    static class UnionFind {
+        int[] parents;
+        int count;
+
+        public UnionFind(int n) {
+            count = n;
+
+            parents = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+            }
+        }
+
+        int find(int x) {
+            // 0 1 2 3 3 5 4
+            if (parents[x] == x) {
+                return parents[x];
+            }
+
+            int i = find(parents[x]);
+            return parents[x] = i;
+        }
+
+        void union(int m, int n) {
+            int rootM = find(m);
+            int rootN = find(n);
+
+            if (rootM == rootN) {
+                return;
+            }
+
+            parents[rootM] = rootN;
+            count--;
+        }
+    }
+
+    public int findCircleNumUnionFind(int[][] isConnected) {
+        int n = isConnected.length;
+        UnionFind union = new UnionFind(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    union.union(i, j);
+                }
+            }
+        }
+
+        return union.count;
+    }
+
+    public static void main(String[] args) {
+        LeetCode_547_Number_of_Provinces solution =
+                new LeetCode_547_Number_of_Provinces();
+
+        int[][] isConnected = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}
+        };
+
+        int provinces = solution.findCircleNumUnionFind(isConnected);
+
+        System.out.println(provinces);
+    }
 }
