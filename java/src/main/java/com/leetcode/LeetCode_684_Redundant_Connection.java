a344fbe7645d7d3439cc2853f4c086a7b0192352
Violet Rosalia <violet.rosalia@gmail.com>
2026-05-11 14:30:42 +0900

    684. Redundant Connection - Union Find

--- java/src/main/java/com/leetcode/LeetCode_684_Redundant_Connection.java
+++ java/src/main/java/com/leetcode/LeetCode_684_Redundant_Connection.java
@@ -1,38 +1,50 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LeetCode_684_Redundant_Connection {
-    public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
-        int[] parents = new int[n + 1];
-        for (int i = 0; i < parents.length; i++) {
-            parents[i] = i;
+    static class UnionFind {
+        int[] parents;
+
+        public UnionFind(int n) {
+            parents = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+            }
         }
 
+        boolean union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX == rootY) {
+                return false;
+            }
+
+            parents[rootX] = rootY;
+
+            return true;
+        }
+
+        int find(int x) {
+            if (parents[x] == x) {
+                return x;
+            }
+
+            return parents[x] = find(parents[x]);
+        }
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n + 1);
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
-            int pu = find(parents, u);
-            int pv = find(parents, v);
-
-            if (pv == pu) {
+            if (!uf.union(u, v)) {
                 return edge;
             }
-
-            parents[pu] = pv;
         }
 
-        return new int[0];
-    }
-
-    private int find(int[] parents, int x) {
-        if (parents[x] == x) {
-            return x;
-        }
-
-        parents[x] = find(parents, parents[x]);
-        return parents[x];
+        return new int[2];
     }
 }
