baded035f645fb8841a139b38d7255837c4cd285
Violet Rosalia <violet.rosalia@gmail.com>
2026-05-12 19:03:32 +0900

    Add DFS solution for redundant connection

--- java/src/main/java/com/leetcode/LeetCode_684_Redundant_Connection.java
+++ java/src/main/java/com/leetcode/LeetCode_684_Redundant_Connection.java
@@ -1,5 +1,10 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LeetCode_684_Redundant_Connection {
     static class UnionFind {
         int[] parents;
@@ -46,5 +51,43 @@ public class LeetCode_684_Redundant_Connection {
         }
 
         return new int[2];
+    }
+
+    public int[] findRedundantConnectionDFS(int[][] edges) {
+        int n = edges.length;
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            boolean[] visited = new boolean[n + 1];
+            if (hasPath(graph, u, v, visited)) {
+                return edge;
+            }
+
+            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+        }
+
+        return new int[]{};
+    }
+
+    private boolean hasPath(Map<Integer, List<Integer>> graph, int current, int target, boolean[] visited) {
+        visited[current] = true;
+        for (int neighbor : graph.getOrDefault(current, new ArrayList<>())) {
+            if (neighbor == target) {
+                return true;
+            }
+
+            if (visited[neighbor]) {
+                continue;
+            }
+
+            if (hasPath(graph, neighbor, target, visited)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
