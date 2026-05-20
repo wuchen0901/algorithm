755b39582c45d349e3571768762d4d373881a789
Violet Rosalia <violet.rosalia@gmail.com>
2026-05-20 16:43:12 +0900

    77. Combinations

---
+++ java/src/main/java/com/leetcode/LeetCode_77_Combinations.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_77_Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        backtrack(n, k, new ArrayList<>(), 1, result);
+
+        return result;
+    }
+
+    void backtrack(int n, int k, List<Integer> path, int start, List<List<Integer>> result) {
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            path.add(i);
+            backtrack(n, k, path, i + 1, result);
+            path.removeLast();
+        }
+    }
+}
