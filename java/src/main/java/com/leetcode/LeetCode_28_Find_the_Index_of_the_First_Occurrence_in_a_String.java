347b8171c3045d824860a5a7b370c7f435717f4c
Violet Rosalia <violet.rosalia@gmail.com>
2026-05-25 17:42:45 +0900

    28. Find the Index of the First Occurrence in a String KMP

--- java/src/main/java/com/leetcode/LeetCode_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
+++ java/src/main/java/com/leetcode/LeetCode_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -18,4 +18,53 @@ public class LeetCode_28_Find_the_Index_of_the_First_Occurrence_in_a_String {
 
         return -1;
     }
+
+    public int strStrKMP(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+
+        int[] lps = buildLPS(needle);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == needle.length()) {
+                    return i - j;
+                }
+            } else if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+
+        return -1;
+    }
+
+    private int[] buildLPS(String needle) {
+        int[] lps = new int[needle.length()];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < needle.length()) {
+            if (needle.charAt(i) == needle.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else if (len > 0) {
+                len = lps[len - 1];
+            } else {
+                i++;
+            }
+        }
+
+        return lps;
+    }
 }
