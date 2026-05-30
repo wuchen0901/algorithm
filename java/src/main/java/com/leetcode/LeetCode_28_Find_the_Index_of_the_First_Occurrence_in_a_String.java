40d349a20815ad90f12784f1b67a6026e4ad935d
Violet Rosalia <violet.rosalia@gmail.com>
2026-05-30 23:20:35 +0900

    28. Find the Index of the First Occurrence in a String KMP

--- java/src/main/java/com/leetcode/LeetCode_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
+++ java/src/main/java/com/leetcode/LeetCode_28_Find_the_Index_of_the_First_Occurrence_in_a_String.java
@@ -22,48 +22,60 @@ public class LeetCode_28_Find_the_Index_of_the_First_Occurrence_in_a_String {
     }
 
     public int strStrKMP(String haystack, String needle) {
-        if (needle.isEmpty()) {
-            return 0;
-        }
-
         int[] lps = buildLPS(needle);
 
+        // i points to the current character in the haystack.
         int i = 0;
+
+        // I initialize j to 0
         int j = 0;
 
+        // I iterate while i is less than the length of the haystack
         while (i < haystack.length()) {
             if (haystack.charAt(i) == needle.charAt(j)) {
                 i++;
                 j++;
 
+                // If j equals the length of the needle.
                 if (j == needle.length()) {
                     return i - j;
                 }
-            } else if (j > 0) {
-                j = lps[j - 1];
             } else {
-                i++;
+                if (0 < j) {
+                    // I set j to LPS at index j - 1.
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
             }
         }
 
         return -1;
     }
 
-    private int[] buildLPS(String needle) {
-        int[] lps = new int[needle.length()];
-
-        int len = 0;
+    private int[] buildLPS(String pattern) {
+        // First, I define an integer array called lps.
+        // Create the LPS array with the same length as the pattern.
+        // Initialize the LPS array.
+        int[] lps = new int[pattern.length()];
+        // I create an integer variable i and initialize it to 1.
         int i = 1;
-
-        while (i < needle.length()) {
-            if (needle.charAt(i) == needle.charAt(len)) {
+        // I create an integer variable len and initialize it to 0;
+        int len = 0;
+        // I iterate while i is less than the length of the pattern.
+        while (i < pattern.length()) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
                 len++;
+                // I set lps[i] to len.
                 lps[i] = len;
                 i++;
-            } else if (len > 0) {
-                len = lps[len - 1];
             } else {
-                i++;
+                if (0 < len) {
+                    // I set len to lps at len minus 1
+                    len = lps[len - 1];
+                } else {
+                    i++;
+                }
             }
         }
 
