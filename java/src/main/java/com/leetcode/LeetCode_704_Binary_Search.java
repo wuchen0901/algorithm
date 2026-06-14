d233abf33b6a10913f21160946eedca2d3690018
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-15 00:57:55 +0900

    704. Binary Search

--- java/src/main/java/com/leetcode/LeetCode_704_Binary_Search.java
+++ java/src/main/java/com/leetcode/LeetCode_704_Binary_Search.java
@@ -1,23 +1,41 @@
 package com.leetcode;
 
 public class LeetCode_704_Binary_Search {
-    public int search(int[] nums, int target) {
+    public int searchHalfOpenInterval(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        // [8,12], target: 12
-        // left = 0, right = 1
+
         while (left <= right) {
-            int mid = (left + right) / 2; // 0
+            int mid = left + (right - left) / 2;
 
             if (target < nums[mid]) {
                 right = mid - 1;
             } else if (nums[mid] < target) {
                 left = mid + 1;
-            } else { // nums[mid] == target
-                return mid;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return -1;
+        return left < nums.length && nums[left] == target ? left : -1;
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (target < nums[mid]) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left < nums.length && nums[left] == target ? left : -1;
     }
 }
