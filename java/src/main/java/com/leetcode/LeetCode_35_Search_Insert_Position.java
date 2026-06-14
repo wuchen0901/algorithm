f5dbe24df579178d049d17cf9861b45f39b57080
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-14 17:45:46 +0900

    35. Search Insert Position

---
+++ java/src/main/java/com/leetcode/LeetCode_35_Search_Insert_Position.java
@@ -0,0 +1,21 @@
+package com.leetcode;
+
+public class LeetCode_35_Search_Insert_Position {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target < nums[mid]) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
