5a6ac6f043f609ffa45a679a98cd9dadeba5d338
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-17 15:45:00 +0900

    340. Longest Substring with At Most K Distinct Characters

--- java/src/main/java/com/leetcode/LeetCode_340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
+++ java/src/main/java/com/leetcode/LeetCode_340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -1,25 +1,29 @@
 package com.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LeetCode_340_Longest_Substring_with_At_Most_K_Distinct_Characters {
 
     int lengthOfLongestSubstringKDistinct(String s, int k) {
-        Map<Character, Integer> window = new HashMap<>();
         int longestLength = 0;
-        int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            window.put(s.charAt(r), window.getOrDefault(s.charAt(r), 0) + 1);
-            // shink
-            while (k < window.size()) {
-                window.compute(s.charAt(l), (character, frequency) -> (frequency == null || frequency == 1) ? null : frequency - 1);
-                l++;
+        int[] freq = new int[128];
+        int distinctCount = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            if (freq[rightChar] == 0) {
+                distinctCount++;
+            }
+            freq[rightChar]++;
+            while (k < distinctCount) {
+                char leftChar = s.charAt(left);
+                freq[leftChar]--;
+                if (freq[leftChar] == 0) {
+                    distinctCount--;
+                }
+
+                left++;
             }
 
-            if (longestLength < r + 1 - l) {
-                longestLength = r + 1 - l;
-            }
+            longestLength = Math.max(longestLength, right + 1 - left);
         }
 
         return longestLength;
