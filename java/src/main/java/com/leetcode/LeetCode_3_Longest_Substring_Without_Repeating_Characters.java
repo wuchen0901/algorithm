b4d4990f2627b9a8c9c56a1676cf1e994c827224
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-17 13:51:29 +0900

    3. Longest Substring Without Repeating Characters

--- java/src/main/java/com/leetcode/LeetCode_3_Longest_Substring_Without_Repeating_Characters.java
+++ java/src/main/java/com/leetcode/LeetCode_3_Longest_Substring_Without_Repeating_Characters.java
@@ -6,70 +6,23 @@ import java.util.Map;
 import java.util.Set;
 
 public class LeetCode_3_Longest_Substring_Without_Repeating_Characters {
-    // expand first
-    public int lengthOfLongestSubstringV3(String s) {
-        Map<Character, Integer> window = new HashMap<>();
-        int l = 0;
+    public int lengthOfLongestSubstring(String s) {
+        int[] windowFreq = new int[128];
         int longestLength = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            windowFreq[rightChar]++;
 
-        for (int r = 0; r < s.length(); r++) {
-            char c = s.charAt(r);
-            window.put(c, window.getOrDefault(c, 0) + 1);
-            // "abcabcbb"
-            //  ^  ^
-            // move left until it doesn't contain duplicates
-            while (1 < window.getOrDefault(c, 0)) {
-                // update window
-                window.computeIfPresent(s.charAt(l), (character, count) -> count == 1 ? null : count - 1);
-                l++;
+            while (1 < windowFreq[rightChar]) {
+                char leftChar = s.charAt(left);
+                windowFreq[leftChar]--;
+                left++;
             }
-            // "abcabcbb"
-            //   ^ ^
-            longestLength = Math.max(longestLength, r - l + 1);
+
+            longestLength = Math.max(longestLength, right + 1 - left);
         }
 
         return longestLength;
     }
-
-    // Shrink first
-    public int lengthOfLongestSubstringV2(String s) {
-        Set<Character> window = new HashSet<>();
-        int l = 0;
-        int longestLength = 0;
-        for (int r = 0; r < s.length(); r++) {
-            // move left until it doesn't contain
-            while (window.contains(s.charAt(r))) {
-                // update window
-                window.remove(s.charAt(l));
-                l++;
-            }
-            window.add(s.charAt(r));
-            longestLength = Math.max(longestLength, r - l + 1);
-        }
-
-        return longestLength;
-    }
-
-    // Instinct but incorrect
-    public int lengthOfLongestSubstringV1(String s) {
-        Set<Character> window = new HashSet<>();
-        int l = 0;
-        int longestLength = 0;
-        for (int r = 0; r < s.length(); r++) {
-            if (window.contains(s.charAt(r))) {
-                // move left until it doesn't contain
-                while (window.contains(s.charAt(r))) {
-                    // update window
-                    window.remove(s.charAt(l));
-                    l++;
-                }
-            } else {
-                window.add(s.charAt(r));
-                longestLength = Math.max(longestLength, r - l);
-            }
-        }
-
-        return longestLength;
-    }
-
 }
