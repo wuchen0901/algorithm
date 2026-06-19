08f075642302f9ab1354ec7fa7afb233d15436b6
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-19 14:14:18 +0900

    15. 3Sum

--- java/src/main/java/com/leetcode/LeetCode_15_3Sum.java
+++ java/src/main/java/com/leetcode/LeetCode_15_3Sum.java
@@ -7,32 +7,45 @@ import java.util.List;
 public class LeetCode_15_3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> results = new ArrayList<>();
 
+        // I would like to use two nested loops.
+        // The outer loop fixes the first number.
+        // The outer loop only needs to iterate up to nums.length - 2
+
+        List<List<Integer>> result = new ArrayList<>();
+        // To avoid duplicates, I need to skip over duplicate values whenever I move any of the three indices.
         for (int i = 0; i < nums.length - 2; i++) {
-            if (1 <= i && nums[i - 1] == nums[i]) {
-                continue;
-            }
-            int l = i + 1;
-            int r = nums.length - 1;
+            // For each iteration of the outer loop, we use two pointers to find the remaining two numbers.
+            // We only need to search for the remaining two numbers to the right of the first number.
 
-            while (l < r) {
-                if (nums[i] + nums[l] + nums[r] < 0) {
-                    l++;
-                } else if (0 < nums[i] + nums[l] + nums[r]) {
-                    r--;
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (0 < sum) {
+                    right--;
                 } else {
-                    results.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    do {
-                        l++;
-                    } while (l < r && nums[l - 1] == nums[l]);
-                    do {
-                        r--;
-                    } while (l < r && nums[r] == nums[r + 1]);
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    while (left < right && nums[left - 1] == nums[left]) {
+                        left++;
+                    }
+                    right--;
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
                 }
+            }
+
+            while (i + 1 < nums.length - 2 && nums[i] == nums[i + 1]) {
+                i++;
             }
         }
 
-        return results;
+        // I always make syntax mistakes in statements like this.
+        return result;
     }
 }
