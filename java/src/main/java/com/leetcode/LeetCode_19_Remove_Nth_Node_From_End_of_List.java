78150bfbaa3ae0355daa70bc7eb997362df42f45
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-23 01:55:04 +0900

    19. Remove Nth Node From End of List

---
+++ java/src/main/java/com/leetcode/LeetCode_19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import com.leetcode.common.ListNode;
+
+public class LeetCode_19_Remove_Nth_Node_From_End_of_List {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            return slow.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}
