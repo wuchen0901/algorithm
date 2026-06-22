ae9324a2bc8cef0e073a783c273702a9ca8d3361
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-23 02:16:16 +0900

    142. Linked List Cycle II

---
+++ java/src/main/java/com/leetcode/LeetCode_142_Linked_List_Cycle_II.java
@@ -0,0 +1,25 @@
+package com.leetcode;
+
+import com.leetcode.common.ListNode;
+
+public class LeetCode_142_Linked_List_Cycle_II {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+
+        return null;
+    }
+}
