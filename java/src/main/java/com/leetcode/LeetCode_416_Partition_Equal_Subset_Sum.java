a8cfec6e90a8c52ef29b65ff96493bea4e84d674
Violet Rosalia <violet.rosalia@gmail.com>
2026-06-24 19:02:02 +0900

    416. Partition Equal Subset Sum

--- java/src/main/java/com/leetcode/LeetCode_416_Partition_Equal_Subset_Sum.java
+++ java/src/main/java/com/leetcode/LeetCode_416_Partition_Equal_Subset_Sum.java
@@ -4,37 +4,26 @@ public class LeetCode_416_Partition_Equal_Subset_Sum {
     public boolean canPartition(int[] nums) {
         int sum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+        for (int k : nums) {
+            sum += k;
         }
 
-        if (sum %2 == 1) {
+        if (sum % 2 == 1) { // odd
             return false;
         }
 
         int target = sum / 2;
-
-        int[][] dp = new int[nums.length + 1][target + 1];
-
-        dp[0][0] = 1;
-
-        //              0   1   2   3   4   5   6
-        //  []          1 // TODO: 为什么这里是 1 ？
-        //  [2]         1       1
-        //  [2, 3]      1       1   1       1
-        //  [2, 3, 1]   1   1   1   2   1   1   1
-        for (int i = 1; i < nums.length + 1; i++) {
-            int num = nums[i - 1];
-            for (int j = 0; j < target + 1; j++) {
-                if (0 <= j - num) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - num];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        // 1, 5, 11, 5
+        // 0 1 2 3 4 5 6 7 8 9 10 11
+        // t t       t t        t  t
+        for (int num : nums) {
+            for (int j = dp.length - 1; 0 <= j - num; j--) {
+                dp[j] |= dp[j - num];
             }
         }
 
-        return dp[nums.length][target] != 0;
+        return dp[target];
     }
 }
