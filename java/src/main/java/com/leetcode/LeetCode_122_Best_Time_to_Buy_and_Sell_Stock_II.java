2fc33bf69520fe7761f4246f5ac34e4c92a6d1b4
Chen.A.Wu <Chen.A.Wu@sony.com>
2026-06-29 15:39:44 +0900

    122. Best Time to Buy and Sell Stock II

--- java/src/main/java/com/leetcode/LeetCode_122_Best_Time_to_Buy_and_Sell_Stock_II.java
+++ java/src/main/java/com/leetcode/LeetCode_122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -2,12 +2,19 @@ package com.leetcode;
 
 public class LeetCode_122_Best_Time_to_Buy_and_Sell_Stock_II {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i - 1] < prices[i]) {
-                profit += prices[i] - prices[i - 1];
-            }
+        int n = prices.length;
+        int[] hold = new int[n + 1];
+        int[] sold = new int[n + 1];
+        hold[0] = Integer.MIN_VALUE;
+        //      7       5       4           8
+        // hold -INF    -7     -5           -5, 0 - 4
+        // sold 0       0       0, -7 + 5   0, -5 + 8
+        for (int i = 1; i < n + 1; i++) {
+            int price = prices[i - 1];
+            hold[i] = Math.max(hold[i - 1], sold[i - 1] - price);
+            sold[i] = Math.max(sold[i - 1], hold[i - 1] + price);
         }
-        return profit;
+
+        return sold[n];
     }
 }
